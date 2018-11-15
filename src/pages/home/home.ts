62af257aeb1ee384ba90d3ad337a6deb62b58d15
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-15 00:35:11 -0600

    added weather data to home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  weather: string;
+  location: {
+    longitude: number,
+    latitude: number
+  };
 
+  constructor(public navCtrl: NavController, private weatherProvider: WeatherProvider) {
+
+  }
+
+  ionViewWillEnter(){
+    this.location = {
+      latitude : 42.3601,
+      longitude : -71.0589
+    };
+
+    this.weatherProvider.getWeather(this.location.latitude, this.location.longitude).subscribe(data => {
+        this.weather = data;
+    });
   }
 
 }
--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class WeatherProvider {
 
-  private apiKey: string = 'some-api-key';
+  private apiKey: string = '786c83b86e5707c2f43374d466e94a2c';
 
   private url: string;
 
@@ -15,7 +16,7 @@ export class WeatherProvider {
     this.url = 'https://api.darksky.net/forecast/'+ this.apiKey;
   }
 
-  getWeather(latitude: number, longitude: number){
+  getWeather(latitude: number, longitude: number): Observable<any>{
     return this.http.get(this.url + '/' + latitude + ',' + longitude).map(res => res);
   }
 
