36d74c94285611b7e688b81b2566f8cebf187dc4
jwyatt123 <justinjwyatt@gmail.com>
2018-12-07 17:36:25 -0600

    added storage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { WeatherProvider } from './../../providers/weather/weather';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -11,14 +12,25 @@ export class HomePage {
   public weather: any;
   public city: string;
 
-  constructor(public navCtrl: NavController, public weatherProvider: WeatherProvider) {
+  constructor(public navCtrl: NavController, public weatherProvider: WeatherProvider, private storage: Storage) {
 
   }
 
   ionViewWillEnter(){
-    this.city = "London";
-    this.weatherProvider.getWeather(this.city).subscribe(weather => {
-      this.weather = weather;
+
+    this.storage.get('location').then((val) => {
+      if(val !== null){
+        this.city = val;
+      }else{
+        this.city = "London";
+      }
+
+      this.weatherProvider.getWeather(this.city).subscribe(weather => {
+        this.weather = weather;
+      });
+
     });
+
+    
   }
 }
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 /**
  * Generated class for the SettingsPage page.
@@ -15,11 +16,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public city: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
+    this.city = 'London';
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
+  }
+
+  saveChanges() {
+
   }
 
 }
