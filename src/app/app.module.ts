794d1ce362cfc3a350955c9766ef8d095b1cbcd8
jwyatt123 <justinjwyatt@gmail.com>
2018-12-07 13:30:39 -0600

    added settings page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SettingsPage } from './settings/settings.page';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SettingsPage],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
